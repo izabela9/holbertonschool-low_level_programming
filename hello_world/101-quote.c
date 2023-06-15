0039963621121bd030fb05ebec4f28b1c1ce44e8
izabela9 <isabelanelaj1@gmail.com>
2023-06-15 07:15:37 -0700

    9unix

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(1, s, sizeof(s) - 1);
-	write(1, "\n", 2);
+	write(1, "\n", 1);
 
 	return (1);
 }
