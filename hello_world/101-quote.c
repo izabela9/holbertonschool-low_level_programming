805c7acfc9d99c3e9fb11db321b8c5ad4cdb2383
izabela9 <isabelanelaj1@gmail.com>
2023-06-15 07:10:35 -0700

    9unix

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, s, sizeof(s));
+	write(1, "\n", 2);
+
+	return (1);
+}
