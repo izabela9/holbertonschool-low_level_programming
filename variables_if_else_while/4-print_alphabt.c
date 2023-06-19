a713d4fc683171dfdc8e3cb7f414cee369ebf835
izabela9 <isabelanelaj1@gmail.com>
2023-06-19 04:32:42 -0700

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+	{
+		if ((letter != 'q') && (letter != 'e'))
+	putchar(letter);
+	}
+	putchar('\n');
+return (0);
+}
