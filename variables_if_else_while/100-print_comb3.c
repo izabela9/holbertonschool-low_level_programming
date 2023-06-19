8e769393f83075fadad86ff70e19c3b4e8185435
izabela9 <isabelanelaj1@gmail.com>
2023-06-19 06:51:36 -0700

    100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,19 @@ int main(void)
 	int i, j;
 
 	for (i = 0 ; i <= 9 ; i++)
-{
-	for (j = 0 ; j <= 9 ; j++)
-		if (i < j && i != j)
-		{
-			putchar('0' + i);
-			putchar('0' + j);
-			if (i != 8 || j != 9)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+			if (i < j && i != j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar('0' + i);
+				putchar('0' + j);
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-}
-}
-putchar('\n');
-return (0);
+	}
+	putchar('\n');
+	return (0);
 }
