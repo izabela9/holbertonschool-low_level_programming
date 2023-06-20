c3f7e60be17b321f54706f56ed3db2c91d1484f8
izabela9 <isabelanelaj1@gmail.com>
2023-06-20 07:33:54 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - it takes an argument c of value integer
+ * @r: integer
+ * Return: last digit of the number
+ */
+int print_last_digit(int r)
+{
+	int lastDigit = r % 10;
+
+	_putchar('0' + lastDigit);
+	return (lastDigit);
+}
