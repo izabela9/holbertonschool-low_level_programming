adebd6ddcc2bd9859e66191a0659ea202ddeb6e5
izabela9 <isabelanelaj1@gmail.com>
2023-06-20 06:25:15 -0700

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isalpha - is a function that checks for alphabetic character
- * @c : is a character
- * Return: 1 if is a letter, lowerscase or uppercase , 0 otherwise
+ * _abs - is a function that computes the apsolute value
+ * @r : integer
+ * Return: r the apsolute number
  */
 int _abs(int r)
 {
