14575e90da4318a783195c210faa1b041b8aedc6
izabela9 <isabelanelaj1@gmail.com>
2023-06-20 06:21:56 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - is a function that checks for alphabetic character
+ * @c : is a character
+ * Return: 1 if is a letter, lowerscase or uppercase , 0 otherwise
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = (-1) * r;
+		return (r);
+	}
+	return (r);
+}
