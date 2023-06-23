b7f098db5980ed15b1e7a68f0704d893721fdc52
izabela9 <isabelanelaj1@gmail.com>
2023-06-23 02:20:16 -0700

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,9 +9,6 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n <= 0)
-		_putchar('\n');
-
 	for (i = 0; i <  n; i++)
 	{
 		for (j = 0; j <= i; j++)
@@ -22,6 +19,8 @@ void print_diagonal(int n)
 			}
 			_putchar(' ');
 		}
+	       	_putchar('\n');
 	}
-	_putchar('\n');
+	 if (n <= 0)
+                _putchar('\n');
 }
