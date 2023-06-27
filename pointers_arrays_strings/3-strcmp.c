f2160323e21094d32e4a970c72984315571f98e3
izabela9 <isabelanelaj1@gmail.com>
2023-06-27 06:50:54 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,12 @@
 int _strcmp(char *s1, char *s2)
 {
     int i;
-    int len = strlen(char *s1);
+    int len = strlen(*s1);
     
     for (i = 0; i < len; i++)
     {
         if (s1[i] - s2[i] != 0)
-        return (s1[] - s2[])
+        return (s1 - s2)
     
     }
     return (0);
