60bcab799864885ab7d568918597f8d5658f5390
izabela9 <isabelanelaj1@gmail.com>
2023-06-27 07:00:32 -0700

    function that compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
-
-
-
+/**
+ *_strcmp -  function that copies a string
+ *@s1: first string to be compared
+ *@s2: string to be compared with s1
+ *Return: returns an integer indicating the result of the
+ *comparison
+*/
 int _strcmp(char *s1, char *s2)
 {
-    int i;
-    int len = strlen(s1);
-    
-    for (i = 0; i < len; i++)
-    {
-        if (s1[i] - s2[i] != 0)
-        return (s1 - s2);
-    
-    }
-    return (0);
+	int i;
+
+	int len = strlen(s1);
+
+	for (i = 0; i < len; i++)
+	{
+		if (s1[i] - s2[i] != 0)
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
