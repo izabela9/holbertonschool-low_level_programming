4f1d73e62115b5eb8f3895db02283fc95035a1dd
izabela9 <isabelanelaj1@gmail.com>
2023-07-03 06:26:31 -0700

    function for prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * is_prime - function
+ * @n: number
+ * @d: divisor number
+ * Return: number
+ */
+int is_prime(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+	if (d * d > n)
+		return (1);
+	return (is_prime(n, d + 1));
+}
+
+/**
+ * is_prime_number - function
+ * @n: inpunt number
+ * Return: number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime(n, 2));
+}
