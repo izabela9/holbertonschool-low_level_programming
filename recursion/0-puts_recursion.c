d7f743f691ed874ae3b228dc40760f8a90a6263a
izabela9 <isabelanelaj1@gmail.com>
2023-07-03 02:37:38 -0700

    function to print a string in recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - function that prints string
+ * @s: character
+ * Return: a string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}
