add08097f26f38c0b2087c4d6184f7ad7a1b4e7d
izabela9 <isabelanelaj1@gmail.com>
2023-07-05 05:25:00 -0700

    adds positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+* main - Entry point
+* @argc: number of argumenst supplied to argv
+* @argv: vectorial array of arguments
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < (int) strlen(argv[i]); j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+			return (1);
+		}
+	}
+		
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
