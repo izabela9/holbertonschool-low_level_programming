817242929b334e1e0432c3d0cfd8e3df683ebeea
izabela9 <isabelanelaj1@gmail.com>
2023-07-17 04:59:32 -0700

    function that initializes vriable

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+		d->name = name;
+	d->age = age;
+	d->owner = owner;
 	}
 }
