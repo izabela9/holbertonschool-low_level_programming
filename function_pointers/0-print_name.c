b90b0ee1531a2c8b418b0e68b83449ced102b7c0
izabela9 <isabelanelaj1@gmail.com>
2023-07-18 05:47:30 -0700

    function to print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_name - function to print a name
+ * @name: name variable
+ * @f: function callback
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
