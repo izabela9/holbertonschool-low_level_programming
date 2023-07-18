eecac42e327c746af87ef305b3f373f085c68ad5
izabela9 <isabelanelaj1@gmail.com>
2023-07-18 06:38:20 -0700

    execution of function given as a parameter on array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * array_iterator - function that executes a function
+ * @array: given array to iterate
+ * @size: size of the array
+ * @action: callback function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
