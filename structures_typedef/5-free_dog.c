f964757d3a29cd16370ad935ec0ccf9231e89811
izabela9 <isabelanelaj1@gmail.com>
2023-07-18 02:01:53 -0700

    function that frees

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees
+ * @d: variable of the structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(name);
+	free(age);
+	free(owner);
+	free(d);
+}
