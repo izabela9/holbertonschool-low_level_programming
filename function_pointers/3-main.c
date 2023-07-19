b5d917250021efda554fb10277b242278e19bc0a
izabela9 <isabelanelaj1@gmail.com>
2023-07-19 02:28:11 -0700

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,6 +20,6 @@ int main (int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-        printf("%d\n", get_op_func(argv[2])(atoi(argv[1], atoi(argv[3])));
+        printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
         return (0);
 }
