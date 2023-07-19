7cd341960df1a702fdaf063189159ada2194af66
izabela9 <isabelanelaj1@gmail.com>
2023-07-19 02:36:24 -0700

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,18 +8,18 @@
  * @argv: argument list
  * Return: Always 0
  */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-        if (argc != 4)
-        {
-                printf("Error\n");
-                exit(98);
-        }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	if (strlen(argv[2]) > 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-        printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-        return (0);
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
