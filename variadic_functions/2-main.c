5a0695d5e644bc3c9f5bb88c4c9b93de0cd0295b
izabela9 <isabelanelaj1@gmail.com>
2023-07-25 01:09:21 -0700

    function that prints string

---
+++ variadic_functions/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}
---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_strings - function that print strings followed by a new line
+ *@n: number of parameters
+ *@separator: string to be printed between numbers
+ *Return: strings separeted by a separator
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	const char *str;
+
+	va_start(args, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			t = va_arg(args, char *);
+			if (separator != NULL)
+			{
+				if (t != NULL)
+					prinft("%s%s", t, separator);
+				else
+				{
+					printf("(nil)%s", separator);
+				}
+			}
+			else
+				printf("%s", t);
+		}
+		t = va_arg(args, char *);
+		if (t != NULL)
+			printf("%s", t);
+		else
+			printf("(nil)");
+	}
+	printf("\n");
+}
