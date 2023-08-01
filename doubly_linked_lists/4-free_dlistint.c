53c3f14711b3be55495f4bfd6975296902630524
izabela9 <isabelanelaj1@gmail.com>
2023-08-01 05:37:52 -0700

    functionthat frees a list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_dlistint - function that frees list
+ * @head: first node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head->next != NULL)
+	{
+		free_dlistint(head->next);
+	}
+	free(head);
+}
