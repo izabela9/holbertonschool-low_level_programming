a17b3ea219f7886297ee5bd2cccc19c22ec46751
izabela9 <isabelanelaj1@gmail.com>
2023-08-08 06:39:55 -0700

    function that reads a text file and prints it to the POSIX standard output

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
